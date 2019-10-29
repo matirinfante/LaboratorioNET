8bdddfff4abe7208557024cbcb878fe9c4a170e2
Matias Infante <maty.infante@gmail.com>
2019-10-29 20:19:32 -0300

    Adding EntityFrameworkTest project

---
+++ EntityFrameworkTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace EntityFrameworkTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MiClase<int> entero = new MiClase<int>(5);
+            entero.Mostrar();
+            MiClase<string> strings = new MiClase<string>("aloH");
+            strings.Mostrar();
+            MiClase<double> dobles = new MiClase<double>(2.0);
+            dobles.Mostrar();
+            entero.Valor = 6;
+            entero.Mostrar();
+            entero.Default();
+            strings.Default();
+            dobles.Default();
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+
+    class MiClase<T>
+    {
+        public T Valor { get; set; }
+
+        public MiClase(T valor)
+        {
+            this.Valor = valor;
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine(this.Valor);
+        }
+
+        public T Default()
+        {
+            this.Valor = default(T);
+            return Valor;
+        }
+    }
+}
