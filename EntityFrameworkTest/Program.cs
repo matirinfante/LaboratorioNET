721e562a06e8ec59c26e7fbec866db708ba6b94a
Matias Infante <maty.infante@gmail.com>
2019-10-31 19:44:53 -0300

    adding delegates and actions

--- EntityFrameworkTest/Program.cs
+++ EntityFrameworkTest/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-using System.Data.Entity.Core.EntityClient;
-using System.Linq;
 
 namespace EntityFrameworkTest
 {
     internal class Program
     {
+        delegate void Referencia(String mensaje);
+
+        delegate void Referencia2(String mensaje);
+
+        delegate string ActionRef<T>(ref string s);
         public static void Main(string[] args)
         {
             MiClase<int> entero = new MiClase<int>(5);
@@ -20,6 +23,38 @@ namespace EntityFrameworkTest
             strings.Default();
             dobles.Default();
             Console.WriteLine("hola".Reversa());
+            var numero = "cinco";
+            Console.WriteLine(numero.Length);
+            Console.ReadKey();
+            var anonimo = new {mensaje = "asfasfa"};
+            Console.WriteLine(anonimo.mensaje);
+            MiClaseParcial parcial = new MiClaseParcial();
+            parcial.imprimir1();
+            parcial.imprimir2();
+            Referencia refDelegado = Console.WriteLine;
+            refDelegado("assfas");
+            Referencia2 refDelegado2 = mensaje => Console.WriteLine();
+            refDelegado2("mensaje 2");
+            Action<String> action = s =>
+            {
+                s = s.Replace(' ', '-');
+                s = s.ToLower();
+                Console.WriteLine(s);
+            };
+            string str = "HOLA COMO ANDAS";
+            action(str);
+            ActionRef<String> actionRef = (ref string s) =>
+            {
+                s = s.Replace(" ", "-");
+                s = s.ToLower();
+                return s;
+            };  
+            Console.WriteLine($"normalito = {str}");
+            actionRef(ref str);
+            Console.WriteLine($"modified by reference = {str}");
+            Func<int, int, int> f = (n1, n2) => n1 * n2;
+            Console.WriteLine(f(1, 2));
+            
         }
     }
 
@@ -34,7 +69,7 @@ namespace EntityFrameworkTest
         }
     }
 
-    class MiClase<T>
+    public class MiClase<T>
     {
         public T Valor { get; set; }
 
@@ -52,6 +87,22 @@ namespace EntityFrameworkTest
         {
             this.Valor = default(T);
             return Valor;
+        }
+    }
+
+    partial class MiClaseParcial
+    {
+        public void imprimir1()
+        {
+            Console.WriteLine("parte 1");
+        }
+    }
+
+    partial class MiClaseParcial
+    {
+        public void imprimir2()
+        {
+            Console.WriteLine("parte 2");
         }
     }
 }
